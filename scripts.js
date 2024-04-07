0c15ba369d1414743aa1a643f130314b1990813f
Marco Aguilar <marcoaguilar@Marcos-MBP-3.lan>
2024-04-07 00:08:48 -0700

    JSON data now immediately loads on loading website. Implementation of array of objects data structure in script.js. Other modifications to css.

--- scripts.js
+++ scripts.js
@@ -5,69 +5,55 @@
  * also make changes to the HTML and CSS files, but we want you to prioritize
  * demonstrating your understanding of data structures, and you'll do that
  * with the JavaScript code you write in this file.
- *
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your
- *    browser and make sure you can see that change.
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- *
  */
 
-const FRESH_PRINCE_URL =
-  "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL =
-  "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL =
-  "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
+// This is an array of objects (Each object is a Video Game)
+// Each object is a video game and its respective data
+let videoGames = [];
 
-// This is an array of strings (TV show titles)
-let titles = [
-  "Fresh Prince of Bel Air",
-  "Curb Your Enthusiasm",
-  "East Los High",
-];
-// Your final submission should have much more data than this, and
-// you should use more than just an array of strings to store it all.
-
+/****
+ * Function will use fetch() to read in data from exterior json file.
+ * Data will then be read into our data structure which will be an
+ * array of objects. Each object being a video game.
+ */
 async function readCsv() {
-  const url = "game_info.json";
   // data from https://www.kaggle.com/datasets/jummyegg/rawg-game-dataset?resource=download
-  const response = await fetch(url);
-  const data = await response.json();
+  const file = "game_info.json";
+  // data from https://www.kaggle.com/datasets/jummyegg/rawg-game-dataset?resource=download
+  const promise = await fetch(file);
+  const data = await promise.json();
   //console.log("Data:", data);
   //console.log("Data.name", data[0].name);
 
+  /*  
   for (let i = 0; i < data.length; i++) {
     //console.log("Pushing: ", data[i].name);
-    titles.push(data[i].name);
-  }
+    videoGames.push(data[i].name);
+  }*/
+  //use forEach loop to push video game object
+  data.forEach((videogame) => {
+    videoGames.push(videogame);
+  });
 
-  ///////
-  //  went from working on a csv file to working on a json file
-  //  in preparation for working with api's. Below is the code for that approach
-  //////
-  // data was text, headersline is now an array of length one with the value at
-  // its only index being the first line of string extracted from data
-  //const headersline = data.split("\n").slice(0, 1); //.slice(start, end): first line element only
-  //console.log("Headers: ", headersline);
+  showCards();
+  /****
+   *  Went from working on a csv file to working on a json file
+   *  in preparation for working with api's. Below is the code for the former
+   ****/
+  /*
+    //data was text, headersline is now an array of length 1 with the value at
+   //its only index being the first line of string extracted from data
+   const headersline = data.split("\n").slice(0, 1); //.slice(start, end): first line element only
+   console.log("Headers: ", headersline);
+   */
 
-  // make array of individual headers
+  /*make array of individual headers
   //const heads = headersline[0].split(",");
 
   //console.log("Heads:", heads, "Size: ", heads.length);
-  /*for (let h = 0; h < heads.length; h++) {
+    for (let h = 0; h < heads.length; h++) {
     console.log("header: ", heads[h]);
-  }*/
+  }
 
   //
   //const rows = data.split("\n").slice(1); //slice(1): start from index 1, skips the headers
@@ -79,57 +65,68 @@ async function readCsv() {
   //  console.log("Row: ", row);
   //});
   //////
-  /*
-    // data from https://www.kaggle.com/datasets/jummyegg/rawg-game-dataset?resource=download
-  const response = await fetch(url);
-  const data = await response.text();
-
-  // data was text, headersline is now an array of length one with the value at
-  // its only index being the first line of string extracted from data
-  const headersline = data.split("\n").slice(0, 1); //.slice(start, end): first line element only
-  //console.log("Headers: ", headersline);
-
-  // make array of individual headers
-  const heads = headersline[0].split(",");
-
-  //console.log("Heads:", heads, "Size: ", heads.length);
-  /*for (let h = 0; h < heads.length; h++) {
-    console.log("header: ", heads[h]);
-  }
-
-  //
-  const rows = data.split("\n").slice(1); //slice(1): start from index 1, skips the headers
-  console.log("Rows size: ", rows.length);
-
-  // for each element in the rows array
-  rows.forEach((element) => {
-    const row = element.split(",");
-    console.log("Row: ", row);
-  });
 */
 }
 
-// This function adds cards the page to display the data in the array
+/* This function adds cards to the page to display the data in the array
+ * @param {}: no parameters
+ * @return
+ */
 function showCards() {
   const cardContainer = document.getElementById("card-container");
   cardContainer.innerHTML = "";
   const templateCard = document.querySelector(".card");
 
-  readCsv();
-  for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
-
-    // This part of the code doesn't scale very well! After you add your
-    // own data, you'll need to do something totally different here.
-    let imageURL = "";
-
-    const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
-    cardContainer.appendChild(nextCard); // Add new card to the container
-  }
+  videoGames.forEach((videoGame) => {
+    let title = videoGame.name;
+    let image = "images/placeholder.png";
+    //the value at the videoGame.updated key, is a string with both a date and a
+    //time that are delimitted by a T character
+    let updated = videoGame.updated.split("T")[0];
+    let released = videoGame.released;
+    let platforms = videoGame.platforms.split("||");
+    let genres = videoGame.genres.split("||");
+    //make a clone of the templateCard
+    const nextCard = templateCard.cloneNode(true);
+    // edit cloned card with the contents from data structure
+    editCardContent(
+      nextCard,
+      title,
+      image,
+      updated,
+      released,
+      platforms,
+      genres,
+    );
+    // add the clone to the cards container
+    cardContainer.appendChild(nextCard);
+  });
+  /*
+  for (let i = 0; i < videoGames.length; i++) {
+    let title = videoGames[i].name;
+  }*/
 }
 
-function editCardContent(card, newTitle, newImageURL) {
+/****
+ * Function will update the cloned HTML container element according to its
+ * cloned children and the data read from our data structure,
+ ****
+ * @param {element} -card: a card element including its children from index.html
+ * @param {string} -newTitle: the title of the video game
+ * @param {string} -newImageURL: url location for video game image. current
+ * location is in local folder images/
+ * @param {string} -updated: the date the video game was last updated
+ * @param {released} -released: the date the video game was released
+ ****/
+function editCardContent(
+  card,
+  newTitle,
+  newImageURL,
+  updated,
+  released,
+  platforms,
+  genres,
+) {
   card.style.display = "block";
 
   const cardHeader = card.querySelector("h2");
@@ -139,14 +136,44 @@ function editCardContent(card, newTitle, newImageURL) {
   cardImage.src = newImageURL;
   cardImage.alt = newTitle + " Cover";
 
-  // You can use console.log to help you debug!
-  // View the output by right clicking on your website,
-  // select "Inspect", then click on the "Console" tab
-  console.log("new card:", newTitle, "- html: ", card);
+  const cardTitle = card.querySelector("li:nth-of-type(1)");
+  cardTitle.textContent += newTitle;
+
+  const cardUpdated = card.querySelector("li:nth-of-type(2)");
+  cardUpdated.textContent += updated;
+
+  const cardReleased = card.querySelector("li:nth-of-type(3)");
+  cardReleased.textContent += released;
+
+  const cardPlatforms = card.querySelector("li:nth-of-type(4)");
+  //index i used for proper placement of comma in text to add
+  let i = 0;
+  platforms.forEach((platform) => {
+    if (i == 0) {
+      cardPlatforms.textContent += platform;
+    } else if (platform == "") {
+      cardPlatforms.textContent += "N/A";
+    } else {
+      cardPlatforms.textContent += ", " + platform;
+    }
+    i++;
+  });
+
+  const cardGenre = card.querySelector("li:nth-of-type(5)");
+  let j = 0;
+  genres.forEach((genre) => {
+    if (j == 0) {
+      cardGenre.textContent += genre;
+    } else {
+      cardGenre.textContent += genre;
+    }
+    j++;
+  });
 }
 
 // This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", showCards);
+document.addEventListener("DOMContentLoaded", readCsv);
+//document.addEventListener("DOMContentLoaded", showCards);
 
 function quoteAlert() {
   console.log("Button Clicked!");
@@ -156,6 +183,6 @@ function quoteAlert() {
 }
 
 function removeLastCard() {
-  titles.pop(); // Remove last item in titles array
+  videoGames.pop(); // Remove last item in videoGames array
   showCards(); // Call showCards again to refresh
 }
