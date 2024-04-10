235c4dc00ee8998b8a100c37961bce0786a2aedd
Marco Aguilar <marcoaguilar@Marcos-MBP-3-31.lan>
2024-04-09 23:14:31 -0700

    Full implementation of requirements. Added screenshots for README display. Updated README to reflect recent changes. Preparing to publish to github pages and submit

--- scripts.js
+++ scripts.js
@@ -1,77 +1,53 @@
-/**
+/****
  * Data Catalog Project Starter Code - SEA Stage 2
  *
  * This file is where you should be doing most of your work. You should
  * also make changes to the HTML and CSS files, but we want you to prioritize
  * demonstrating your understanding of data structures, and you'll do that
  * with the JavaScript code you write in this file.
- */
-
-// This is an array of objects (Each object is a Video Game)
-// Each object is a video game and its respective data
-let videoGames = [];
+ *
+ * -Snap Inc.
+ ****/
 
 /****
- * Function will use fetch() to read in data from exterior json file.
+ * This is an array of objects
+ * Each object is a video game and its respective data
+ ****/
+let videoGames = [];
+
+/*calls the readCsv() function when the page is first loaded*/
+document.addEventListener("DOMContentLoaded", readCsv);
+
+/****
+ * Function will use fetch() to read in data from local json file to initially
+ * populate the videoGames array.
  * Data will then be read into our data structure which will be an
  * array of objects. Each object being a video game.
- */
+ * Data is from https://www.kaggle.com/datasets/jummyegg/rawg-game-dataset?resource=download
+ ****/
 async function readCsv() {
-  // data from https://www.kaggle.com/datasets/jummyegg/rawg-game-dataset?resource=download
   const file = "game_info.json";
-  // data from https://www.kaggle.com/datasets/jummyegg/rawg-game-dataset?resource=download
   const promise = await fetch(file);
   const data = await promise.json();
   //console.log("Data:", data);
   //console.log("Data.name", data[0].name);
 
-  /*  
-  for (let i = 0; i < data.length; i++) {
-    //console.log("Pushing: ", data[i].name);
-    videoGames.push(data[i].name);
-  }*/
-  //use forEach loop to push video game object
+  //forEach loop to push video game object
+  //into videoGames array
   data.forEach((videogame) => {
     videoGames.push(videogame);
   });
 
+  //Call showCards() function to display the data that was saved
+  //into our videoGames data structure variable
   showCards();
-  /****
-   *  Went from working on a csv file to working on a json file
-   *  in preparation for working with api's. Below is the code for the former
-   ****/
-  /*
-    //data was text, headersline is now an array of length 1 with the value at
-   //its only index being the first line of string extracted from data
-   const headersline = data.split("\n").slice(0, 1); //.slice(start, end): first line element only
-   console.log("Headers: ", headersline);
-   */
-
-  /*make array of individual headers
-  //const heads = headersline[0].split(",");
-
-  //console.log("Heads:", heads, "Size: ", heads.length);
-    for (let h = 0; h < heads.length; h++) {
-    console.log("header: ", heads[h]);
-  }
-
-  //
-  //const rows = data.split("\n").slice(1); //slice(1): start from index 1, skips the headers
-  //console.log("Rows size: ", rows.length);
-
-  // for each element in the rows array
-  //rows.forEach((element) => {
-  //  const row = element.split(",");
-  //  console.log("Row: ", row);
-  //});
-  //////
-*/
 }
 
-/* This function adds cards to the page to display the data in the array
+/****
+ * Function clears the HTML element with the id card-container and adds
+ * cards to the page to display the data in the videoGame array
  * @param {}: no parameters
- * @return
- */
+ ****/
 function showCards() {
   const cardContainer = document.getElementById("card-container");
   cardContainer.innerHTML = "";
@@ -86,8 +62,10 @@ function showCards() {
     let released = videoGame.released;
     let platforms = videoGame.platforms.split("||");
     let genres = videoGame.genres.split("||");
+
     //make a clone of the templateCard
     const nextCard = templateCard.cloneNode(true);
+
     // edit cloned card with the contents from data structure
     editCardContent(
       nextCard,
@@ -98,25 +76,57 @@ function showCards() {
       platforms,
       genres,
     );
-    // add the clone to the cards container
+
+    // add the edited clone to the cards container
     cardContainer.appendChild(nextCard);
   });
-  /*
-  for (let i = 0; i < videoGames.length; i++) {
-    let title = videoGames[i].name;
-  }*/
 }
+
+/****
+ * Function will pop() the last videoGame object in the array.
+ * The videoGame cards in the array will then be redisplayed
+ * to show the changes
+ ****/
+function removeLastCard() {
+  videoGames.pop(); // Remove last item in videoGames array
+  showCards(); // Call showCards again to refresh
+}
+
+/****
+ * Went from working on a csv file to working on a json file
+ * in preparation for possible work with api's.
+ * Below was some code for the former attempt.
+ ****/
+/*
+ //data was text, headersline an array of length 1 with the value at
+ //its only index being the first line of string extracted from data
+ 
+ const headersline = data.split("\n").slice(0, 1); //.slice(start, end): first line only
+ //make array of individual headers
+ const heads = headersline[0].split(",")
+
+ //Rows
+  const rows = data.split("\n").slice(1); //slice(1): start from index 1, skips the headers
+
+  //for each element in the rows array
+  rows.forEach((element) => {
+    const row = element.split(",");
+  });
+
+  //Cols... */
 
 /****
  * Function will update the cloned HTML container element according to its
  * cloned children and the data read from our data structure,
  ****
- * @param {element} -card: a card element including its children from index.html
- * @param {string} -newTitle: the title of the video game
- * @param {string} -newImageURL: url location for video game image. current
+ * @card {html element} : a card html element including its children from index.html
+ * @newTitle {string} : the title of the video game
+ * @newImageURL {string} : url location for video game image. current
  * location is in local folder images/
- * @param {string} -updated: the date the video game was last updated
- * @param {released} -released: the date the video game was released
+ * @updated {string} : the date the video game was last updated
+ * @released {string} : the date the video game was released
+ * @platforms {array} : each platform the video game is compatible with
+ * @genres {array} : the genres the video game belongs to
  ****/
 function editCardContent(
   card,
@@ -129,6 +139,8 @@ function editCardContent(
 ) {
   card.style.display = "block";
 
+  //set the content of the card parameter to the values
+  //of the other parameters
   const cardHeader = card.querySelector("h2");
   cardHeader.textContent = newTitle;
 
@@ -137,22 +149,22 @@ function editCardContent(
   cardImage.alt = newTitle + " Cover";
 
   const cardTitle = card.querySelector("li:nth-of-type(1)");
-  cardTitle.textContent += newTitle;
+  cardTitle.textContent += " " + newTitle;
 
   const cardUpdated = card.querySelector("li:nth-of-type(2)");
-  cardUpdated.textContent += updated;
+  cardUpdated.textContent += " " + updated;
 
   const cardReleased = card.querySelector("li:nth-of-type(3)");
-  cardReleased.textContent += released;
+  cardReleased.textContent += " " + released;
 
   const cardPlatforms = card.querySelector("li:nth-of-type(4)");
-  //index i used for proper placement of comma in text to add
+  //forEach loop to add each platform in the platforms array with commas
+  //index i used for proper placement of comma in text to be appended
   let i = 0;
   platforms.forEach((platform) => {
     if (i == 0) {
-      cardPlatforms.textContent += platform;
-    } else if (platform == "") {
-      cardPlatforms.textContent += "N/A";
+      cardPlatforms.textContent += " " + platform;
+      i++;
     } else {
       cardPlatforms.textContent += ", " + platform;
     }
@@ -162,27 +174,118 @@ function editCardContent(
   const cardGenre = card.querySelector("li:nth-of-type(5)");
   let j = 0;
   genres.forEach((genre) => {
-    if (j == 0) {
-      cardGenre.textContent += genre;
+    if (genre == "" || genre == " ") {
+      cardGenre.textContent += " N/A";
+      j++;
+    } else if (j == 0) {
+      cardGenre.textContent += " " + genre;
+      j++;
     } else {
-      cardGenre.textContent += genre;
+      cardGenre.textContent += ", " + genre;
     }
     j++;
   });
 }
 
-// This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", readCsv);
-//document.addEventListener("DOMContentLoaded", showCards);
+/****
+ * Function will trigger when pressing the "Add Game" button in index.html
+ * Function will clear the html element with id "card-container" of all other
+ * HTML elements. Function will then create a fieldset and appropriate elements
+ * to receive user input for the new game to add to our data structure.
+ ****/
+function addGame() {
+  //first we need to empty the card-container to display our input form instead
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = "";
 
-function quoteAlert() {
-  console.log("Button Clicked!");
-  alert(
-    "I guess I can kiss heaven goodbye, because it got to be a sin to look this good!",
-  );
+  //create fieldset and all its elements and append them to the cardContainer
+  const field = document.createElement("fieldset");
+  field.setAttribute("id", "newGameData");
+
+  const legend = document.createElement("legend");
+  legend.innerText = "Enter Game Details";
+  field.appendChild(legend);
+
+  const titleInput = document.createElement("input");
+  titleInput.setAttribute("type", "text");
+  titleInput.setAttribute("id", "newTitle");
+  titleInput.setAttribute("placeholder", "Title");
+  titleInput.setAttribute("max", "20");
+  titleInput.setAttribute("maxlength", "40");
+  field.appendChild(titleInput);
+  cardContainer.innerHTML += "<br>";
+  cardContainer.innerHTML += "<br>";
+
+  const updatedInput = document.createElement("input");
+  updatedInput.setAttribute("type", "text");
+  updatedInput.setAttribute("id", "newUpdated");
+  updatedInput.setAttribute("placeholder", "Updated Date: mm/dd/yy");
+  updatedInput.setAttribute("max", "20");
+  updatedInput.setAttribute("maxlength", "40");
+  field.appendChild(updatedInput);
+  cardContainer.innerHTML += "<br>";
+  cardContainer.innerHTML += "<br>";
+
+  const releasedInput = document.createElement("input");
+  releasedInput.setAttribute("type", "text");
+  releasedInput.setAttribute("id", "newReleased");
+  releasedInput.setAttribute("placeholder", "Release Date: mm/dd/yy");
+  releasedInput.setAttribute("max", "20");
+  releasedInput.setAttribute("maxlength", "40");
+  field.appendChild(releasedInput);
+
+  const platformInput = document.createElement("input");
+  platformInput.setAttribute("type", "text");
+  platformInput.setAttribute("id", "newPlatform");
+  platformInput.setAttribute("placeholder", "Platform");
+  platformInput.setAttribute("max", "20");
+  platformInput.setAttribute("maxlength", "40");
+  field.appendChild(platformInput);
+  cardContainer.innerHTML += "<br>";
+  cardContainer.innerHTML += "<br>";
+
+  const genreInput = document.createElement("input");
+  genreInput.setAttribute("type", "text");
+  genreInput.setAttribute("id", "newGenre");
+  genreInput.setAttribute("placeholder", "Genre");
+  genreInput.setAttribute("max", "20");
+  genreInput.setAttribute("maxlength", "40");
+  field.appendChild(genreInput);
+  cardContainer.innerHTML += "<br>";
+  cardContainer.innerHTML += "<br>";
+
+  const submitBtn = document.createElement("button");
+  submitBtn.setAttribute("onclick", "pushGame()");
+  submitBtn.innerText = "Add Game";
+  field.appendChild(submitBtn);
+
+  cardContainer.appendChild(field);
 }
 
-function removeLastCard() {
-  videoGames.pop(); // Remove last item in videoGames array
-  showCards(); // Call showCards again to refresh
+/****
+ * Function will trigger when user presses the Add Game button in the displayed
+ * fieldset. Function adds the new game with the user input to the videoGames
+ * array. Function will then call showCards() to display the changes that were
+ * made.
+ ****/
+function pushGame() {
+  //needs to get data from user input for new game to add
+  const newTitle = document.getElementById("newTitle").value;
+  const newUpdated = document.getElementById("newUpdated").value;
+  const newReleased = document.getElementById("newReleased").value;
+  const newPlatforms = document.getElementById("newPlatform").value;
+  const newGenre = document.getElementById("newGenre").value;
+  //console.log("NEW:\n", newTitle, newReleased, newPlatform, newGenre);
+
+  //push the new game to the videoGames array
+  videoGames.push({
+    name: newTitle,
+    updated: newUpdated,
+    released: newReleased,
+    platforms: newPlatforms,
+    genres: newGenre,
+  });
+
+  //show the new game in the videoGames array
+  showCards();
 }
